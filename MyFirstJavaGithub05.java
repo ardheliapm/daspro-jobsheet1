39b59ea4e27e1b767759d79b4b9c09fed36b9933
dedelduwell <ardheliamputri@gmail.com>
2023-09-26 19:02:31 +0700

    mulai fitur kedua

--- MyFirstJavaGithub05.java
+++ MyFirstJavaGithub05.java
@@ -1,5 +1,7 @@
 public class MyFirstJavaGithub05{
     public static void main(String[] args) {
         System.out.println("Ini ceritanya fitur pertama sudah berjalan dengan baik");
+
+        System.out.println("Ketua mulai mengembangkan fitur kedua");
     }
 }
